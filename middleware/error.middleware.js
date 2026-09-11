9c062181a97401ec5f17a2f5a102cac1dbeb293e
Siddhant Rana <siddhantrana.sr.official@gmail.com>
2026-09-12 03:00:49 +0530

    Modified the central error handler middlware to ensure that it logs the error and it's trace in the console.

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,4 +1,21 @@
+// export function errorHandler(error, req, res, next) {
+//   if (res.headersSent) {
+//     return next(error);
+//   }
+
+//   return res.status(error.statusCode || 502).json({
+//     success: false,
+//     message: error.message || 'Something went wrong'
+//   });
+// }
+
 export function errorHandler(error, req, res, next) {
+  console.error(
+    `Error during ${req.method} ${req.originalUrl}`
+  );
+
+  console.error(error);
+
   if (res.headersSent) {
     return next(error);
   }
